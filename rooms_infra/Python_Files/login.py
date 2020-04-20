dee63f0dd661f83e9e64a9361169f3b946459897
Giannis <gchatz@mit.edu>
2020-04-20 17:16:28 +0300

    host_room

--- rooms_infra/Python_Files/login.py
+++ rooms_infra/Python_Files/login.py
@@ -11,6 +11,7 @@ def request_handler(request):
         args = request['form']
         username = str(args['username'])
 
+        create_db()
         conn = sqlite3.connect(db)  # connect to that database (will create if it doesn't already exist)
         c = conn.cursor()  # move cursor into database (allows us to execute commands)
         c.execute('''INSERT into users VALUES (?,?,?,?);''', (username, -1, -1, datetime.datetime.now()))
@@ -21,3 +22,12 @@ def request_handler(request):
 
 
 
+def create_db():
+    #only run once
+    conn = sqlite3.connect(db)  # connect to that database (will create if it doesn't already exist)
+    c = conn.cursor()  # move cursor into database (allows us to execute commands)
+    c.execute('''CREATE TABLE IF NOT EXISTS users (username text, room_id int, game_id int, last_ping timestamp);''')
+    c.execute('''CREATE TABLE IF NOT EXISTS rooms (room_id text, host_username text, capacity int, game_id int, open_time timestamp);''')
+    c.execute('''CREATE TABLE IF NOT EXISTS games (game_id int, room_id int, capacity int, start_time timestamp);''')
+    conn.commit()  # commit commands
+    conn.close()  # close connection to database
