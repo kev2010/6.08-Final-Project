fd417f1bb88347133e821a35b9a34cb98348d761
Baptiste <baptiste@mit.edu>
2020-04-26 22:00:46 +0100

    as

--- rooms_infra/Python_Files/ping.py
+++ rooms_infra/Python_Files/ping.py
@@ -120,12 +120,14 @@ def delete_room(room_id):
     c = conn.cursor()  # move cursor into database (allows us to execute commands)
 
     result = c.execute("SELECT * FROM users WHERE room_id=?", (room_id,)).fetchall()
+
+    result2 = c.execute("SELECT * FROM users").fetchall()
+    return len(result2)
     for r in result:
         #remove them from game and room
         user = r[0]
         c.execute("UPDATE users SET game_id = ? WHERE username =?", (-1, user))
-        # c.execute("UPDATE users SET room_id = ? WHERE username =?", (-1, user))
-        return "lol"
+        c.execute("UPDATE users SET room_id = ? WHERE username =?", (-1, user))
 
     return "hi"
     #delete the game and room
