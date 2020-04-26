580a7f8fcfee9eb47d3a8cc954678816457bb732
Baptiste <baptiste@mit.edu>
2020-04-26 20:53:14 +0100

    change

--- rooms_infra/Python_Files/ping.py
+++ rooms_infra/Python_Files/ping.py
@@ -35,7 +35,7 @@ def request_handler(request):
         c = conn.cursor()  # move cursor into database (allows us to execute commands)
 
         result = c.execute("SELECT * FROM users WHERE username=?", (username,))
-
+        return result
         if len(result) == 0:
             conn = sqlite3.connect(db)  # connect to that database (will create if it doesn't already exist)
             c = conn.cursor()  # move cursor into database (allows us to execute commands)
