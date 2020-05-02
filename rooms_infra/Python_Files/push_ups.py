4a6c4f1441e23c45a03d0e05e2cf67847be57ef7
Baptiste <baptiste@mit.edu>
2020-05-02 22:18:39 +0100

    ch

--- rooms_infra/Python_Files/push_ups.py
+++ rooms_infra/Python_Files/push_ups.py
@@ -47,14 +47,14 @@ def request_handler(request):
         result = c.execute('''SELECT * FROM push_ups WHERE room_id = ?''', (room_id,)).fetchall()
 
         return result
-        result.sort(key=lambda x: x[2])
+        # result.sort(key=lambda x: x[2])
 
         conn.commit()  # commit commands
         conn.close()  # close connection to database
 
         leaderboard = "The leaderboard:\n"
 
-        for i in range(len(result)-1, max(0,len(result)-5), -1):
+        for i in range(len(result)-1, max(-1,len(result)-5), -1):
             r = result[i]
             leaderboard += r[1] + " with score " + str(r[2]) + "\n"
 
