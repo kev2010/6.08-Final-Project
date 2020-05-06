efae73bb3b2db9ffa6d93596b71c3ba514b024a9
Kevin Jiang <kev2018@mit.edu>
2020-05-06 01:54:24 -0400

    test

--- poker-game/website.py
+++ poker-game/website.py
@@ -152,7 +152,7 @@ def request_handler(request):
                     xhttp.onreadystatechange = function() {
                         if (this.readyState == 4 && this.status == 200) {
                             // XMLHttp will provide the servers response as text, we need to parse to turn it into JSON
-                            console.log(response);
+                            console.log(this.response);
                             let response = JSON.parse(this.response); // 89
 
                             // Press f12 to see the console.log and see the full response body from the jokes api, there
