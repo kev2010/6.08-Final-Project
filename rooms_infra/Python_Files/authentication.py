4fd4ee3ead03e058817d8cad97b9c93e8ba47e41
Baptiste <baptiste@mit.edu>
2020-05-12 00:27:47 +0100

    h

---
+++ rooms_infra/Python_Files/authentication.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+db = '__HOME__/project.db'
+
+def request_handler(request):
+    if request['method'] == "GET":
+        username = request['values']['username']
+        password = request['values']['password']
+
+        conn = sqlite3.connect(db)  # connect to that database (will create if it doesn't already exist)
+        c = conn.cursor()  # move cursor into database (allows us to execute commands)
+
+        result = c.execute('''SELECT * FROM users WHERE username = ? AND password = ?''', (username, password))
+
+        conn.commit()  # commit commands
+        conn.close()  # close connection to database
+
+        if len(result) == 0:
+            return 0
+        else:
+            return 1
