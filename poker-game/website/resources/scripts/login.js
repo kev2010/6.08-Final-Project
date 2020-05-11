fea487dcb4f5aeb412806f703ce9fd6534ddff32
Kevin Jiang <kev2018@mit.edu>
2020-05-11 18:56:50 -0400

    Test cookies

--- poker-game/website/resources/scripts/login.js
+++ poker-game/website/resources/scripts/login.js
@@ -1,4 +1,4 @@
-import { setCookie } from './cookies.js';
+// import { setCookie } from './cookies.js';
 
 // Get the modal
 var modal = document.getElementById('id01');
@@ -17,30 +17,40 @@ document.getElementById('login').onsubmit = function() {
     if (isValidLogin(username, password)) {
         setCookie('user', username, 365);
     }
-    
+
     return false;
 }
 
 const isValidLogin = (username, password) => {
-    let xhttp = new XMLHttpRequest();
-    var params = `username=${username}&password=${password}`;
-    //  TODO: CHANGE THIS URL
-    let url = "http://608dev-2.net/sandbox/sc/team079/team079/poker-game/request_handler.py";
+    return true;
+    // let xhttp = new XMLHttpRequest();
+    // var params = `username=${username}&password=${password}`;
+    // //  TODO: CHANGE THIS URL
+    // let url = "http://608dev-2.net/sandbox/sc/team079/team079/poker-game/request_handler.py";
     
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // XMLHttp will provide the servers response as text,s we need to parse to turn it into JSON
-            let response = JSON.parse(this.response); // 89
+    // xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         // XMLHttp will provide the servers response as text,s we need to parse to turn it into JSON
+    //         let response = JSON.parse(this.response); // 89
 
-            // Press f12 to see the console.log and see the full response body from the poker api
-            console.log(response);
-            let to_display = JSON.stringify(response);
-            let valid = respone.valid;
-            return (valid === "True");
-        } else {
-            throw "Login GET request failed!";
-        }
-    }
-    xhttp.open("GET", url+"?"+params, true);
-    xhttp.send(null);
+    //         // Press f12 to see the console.log and see the full response body from the poker api
+    //         console.log(response);
+    //         let to_display = JSON.stringify(response);
+    //         let valid = respone.valid;
+    //         return (valid === "True");
+    //     } else {
+    //         throw "Login GET request failed!";
+    //     }
+    // }
+    // xhttp.open("GET", url+"?"+params, true);
+    // xhttp.send(null);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log("asdfs");
+    console.log(document.cookie);
 }
