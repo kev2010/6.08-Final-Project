7bac421a40aa5862824537607c867a2e0ae60161
Baptiste <baptiste@mit.edu>
2020-05-12 16:09:51 +0100

    h

--- rooms_infra/Python_Files/helpers.py
+++ rooms_infra/Python_Files/helpers.py
@@ -7,7 +7,7 @@ def create_db(conn, c):
     #only run once
     # conn = sqlite3.connect(db)  # connect to that database (will create if it doesn't already exist)
     # c = conn.cursor()  # move cursor into database (allows us to execute commands)
-    c.execute('''CREATE TABLE IF NOT EXISTS users (username text, room_id int, game_id int, last_ping timestamp);''')
+    c.execute('''CREATE TABLE IF NOT EXISTS users (username text, room_id int, game_id int, last_ping timestamp, password text);''')
     c.execute('''CREATE TABLE IF NOT EXISTS rooms (room_id text, host_username text, capacity int, game_id int, open_time timestamp);''')
     c.execute('''CREATE TABLE IF NOT EXISTS games (game_id int, room_id int, capacity int, start_time timestamp);''')
     c.execute('''CREATE TABLE IF NOT EXISTS push_ups (room_id text, username text, score int);''')
