b6e7518f30b444f927dab10265f18022da44f46d
Kevin Jiang <kev2018@mit.edu>
2020-05-11 20:22:34 -0400

    Implement isValidLogin

--- poker-game/website/resources/scripts/login.js
+++ poker-game/website/resources/scripts/login.js
@@ -26,28 +26,27 @@ document.getElementById('login').onsubmit = function() {
 }
 
 const isValidLogin = (username, password) => {
-    return true;
-    // let xhttp = new XMLHttpRequest();
-    // var params = `username=${username}&password=${password}`;
-    // //  TODO: CHANGE THIS URL
-    // let url = "http://608dev-2.net/sandbox/sc/team079/team079/poker-game/request_handler.py";
+    let xhttp = new XMLHttpRequest();
+    var params = `username=${username}&password=${password}`;
+    //  TODO: CHANGE THIS URL
+    let url = "http://608dev-2.net/sandbox/sc/team079/team079/rooms_infra/Python_Files/authentication.py?";
     
-    // xhttp.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         // XMLHttp will provide the servers response as text,s we need to parse to turn it into JSON
-    //         let response = JSON.parse(this.response); // 89
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // XMLHttp will provide the servers response as text,s we need to parse to turn it into JSON
+            let response = JSON.parse(this.response); // 89
 
-    //         // Press f12 to see the console.log and see the full response body from the poker api
-    //         console.log(response);
-    //         let to_display = JSON.stringify(response);
-    //         let valid = respone.valid;
-    //         return (valid === "True");
-    //     } else {
-    //         throw "Login GET request failed!";
-    //     }
-    // }
-    // xhttp.open("GET", url+"?"+params, true);
-    // xhttp.send(null);
+            // Press f12 to see the console.log and see the full response body from the poker api
+            console.log(response);
+            let to_display = JSON.stringify(response);
+            let valid = respone.valid;
+            return (valid === "True");
+        } else {
+            throw "Login GET request failed!";
+        }
+    }
+    xhttp.open("GET", url+params, true);
+    xhttp.send(null);
 }
 
 function setCookie(cname, cvalue, exdays) {
